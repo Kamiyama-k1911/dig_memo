0a6002d0737f258f5c93b977bc83c753c3bcf05d
Kota Kamiyama <k.kamikami1211@gmail.com>
2020-09-21 23:35:55 +0900

    アカウント情報の編集と削除機能を実装。また、そのテストも実装。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -36,22 +36,32 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+    # If you have extra params to permit, append them to the sanitizer.
+    # def configure_sign_up_params
+    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    # end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+    # If you have extra params to permit, append them to the sanitizer.
+    # def configure_account_update_params
+    #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    # end
 
-  # The path used after sign up.
-  def after_sign_up_path_for(resource)
-    articles_path
-  end
+    # The path used after sign up.
+    def after_sign_up_path_for(resource)
+      articles_path
+    end
+
+    # 更新時パスワード入力しなくていいように設定
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
+
+    # 更新後のパス
+    def after_update_path_for(resource)
+      articles_path
+    end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
--- spec/features/resistrations_spec.rb
+++ spec/features/resistrations_spec.rb
@@ -1,16 +1,63 @@
 require "rails_helper"
 
-RSpec.describe "Resistrations", type: :feature do
-  it "新規登録する" do
-    visit new_user_registration_path
+RSpec.describe "Resistrations", js: true, type: :feature do
+  context "新規登録していない場合" do
+    it "新規登録する" do
+      visit new_user_registration_path
 
-    fill_in "ユーザー名", with: "satoshi"
-    fill_in "メールアドレス", with: "satoshi@example.com"
-    fill_in "パスワード", with: "satoshi1200"
-    fill_in "パスワード確認", with: "satoshi1200"
+      fill_in "ユーザー名", with: "satoshi"
+      fill_in "メールアドレス", with: "satoshi@example.com"
+      fill_in "パスワード", with: "satoshi1200"
+      fill_in "パスワード確認", with: "satoshi1200"
 
-    click_on "登録する"
+      click_on "登録する"
 
-    expect(page).to have_content "アカウント登録が完了しました。"
+      expect(page).to have_content "アカウント登録が完了しました。"
+    end
+
+    it "アカウント情報を編集できない" do
+      visit edit_user_registration_path
+
+      expect(page).to have_content "アカウント登録もしくはログインしてください。"
+    end
+  end
+
+  context "新規登録が済んでいる場合" do
+    before do
+      visit new_user_registration_path
+
+      fill_in "ユーザー名", with: "satoshi"
+      fill_in "メールアドレス", with: "satoshi@example.com"
+      fill_in "パスワード", with: "satoshi1200"
+      fill_in "パスワード確認", with: "satoshi1200"
+
+      click_on "登録する"
+    end
+
+    it "アカウント情報を更新できる" do
+      visit edit_user_registration_path
+
+      fill_in "ユーザー名", with: "takeshi"
+      fill_in "メールアドレス", with: "takeshi@example.com"
+
+      click_on "編集する"
+
+      expect(page).to have_content "アカウント情報を変更しました。"
+
+      visit edit_user_registration_path
+
+      expect(page).to have_field "ユーザー名", with: "takeshi"
+      expect(page).to have_field "メールアドレス", with: "takeshi@example.com"
+    end
+
+    it "アカウント情報を削除できる" do
+      visit edit_user_registration_path
+
+      accept_confirm do
+        click_button "アカウントを削除する"
+      end
+
+      expect(page).to have_content "アカウントを削除しました。またのご利用をお待ちしております。"
+    end
   end
 end
