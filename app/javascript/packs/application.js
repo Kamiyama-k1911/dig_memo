855fcdc3387cfb6d0d868990a7bf18507a1e313b
Kota Kamiyama <k.kamikami1211@gmail.com>
2020-09-22 15:13:29 +0900

    アイコンのチラつきを無くすため、fontawesomeの設定を変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,12 +6,12 @@
 //= require popper
 //= require bootstrap-sprockets
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
-require("./test.js")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("jquery");
+require("./test.js");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,6 +20,8 @@ require("./test.js")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import '@fortawesome/fontawesome-free/js/all';
-import 'bootstrap'
-import '../stylesheets/application.scss'
+import "@fortawesome/fontawesome-free/js/all";
+import "bootstrap";
+import "../stylesheets/application.scss";
+
+global.FontAwesome.config.mutateApproach = "sync";
