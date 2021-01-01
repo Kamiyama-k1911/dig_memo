047793131e7daba2dbedf334895562c96ec9fbc7
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-01 13:37:57 +0000

    add: 本番環境での変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,13 +6,11 @@
 //= require popper
 //= require bootstrap-sprockets
 
+require("jquery");
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-require("jquery");
-
-window.$ = $;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,7 +18,8 @@ window.$ = $;
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
+import jquery from 'jquery';
+window.$ = window.jquery = jquery;
 
 import "@fortawesome/fontawesome-free/js/all";
 import "bootstrap";
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "noreply@digmemo-app.xyz"
   layout "mailer"
 end
