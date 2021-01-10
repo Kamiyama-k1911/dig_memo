a5278f372b43eda7aa5607b7099c422c4f122a0b
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-11 07:20:40 +0900

    fix: environment.rbを修正

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,9 @@ require_relative "application"
 Rails.application.initialize!
 
 Capybara.register_driver :selenium do |app|
-  chrome_options = ::Selenium::WebDriver::Chrome::Options.new()
-  chrome_options.add_argument('--no-sandbox')
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new()
+  browser_options.args << '--headless'
+  browser_options.args << '--no-sandbox'
+  browser_options.args << '--disable-gpu'
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 end
