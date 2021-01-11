2953a81eab8b6d51278c01bc64f32b626aedf2a7
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-12 02:47:59 +0900

    fix: 環境変数の有無でheadlessの状態を変える

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,9 @@ Rails.application.initialize!
 
 Capybara.register_driver :selenium do |app|
   browser_options = ::Selenium::WebDriver::Chrome::Options.new
-  browser_options.args << "--headless"
+  if ENV['HEADLESS']
+    browser_options.args << "--headless"
+  end
   browser_options.args << "--no-sandbox"
   browser_options.args << "--disable-gpu"
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
